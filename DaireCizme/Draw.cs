b3473768455c8bd54ed2a1d5ceb164fa5b8ef047
Caner Borazan <caner.borazan@lunatr.com>
2022-10-19 11:20:33 +0300

    Add project files.

---
+++ DaireCizme/Draw.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DaireCizme
+{
+    public class Draw
+    {
+        public void Circle(double radius)
+        {
+            double thickness = 0.4;
+            Console.WriteLine();
+            double rin = radius - thickness, rOut = radius + thickness;
+            for (double y = radius; y >= -radius; --y)
+            {
+                for (double x = -radius; x < rOut; x += 0.5)
+                {
+                    double value = x * x + y * y;
+                    if (value >= rin * rin && value <= rOut * rOut)
+                    {
+                        Console.Write("*");
+                    }
+                    else
+                    {
+                        Console.Write(" ");
+                    }
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ DaireCizme/Program.cs
@@ -0,0 +1,5 @@
+﻿using DaireCizme;
+
+int radius = Convert.ToInt32(Console.ReadLine());
+Draw d = new Draw();
+d.Circle(radius);
